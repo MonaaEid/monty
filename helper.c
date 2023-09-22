be8f919f37f599839e20746f93dd97021f772e5d
MonaaEid <monaa_eid@hotmail.com>
2023-09-22 07:20:43 -0700

    test

--- helper.c
+++ helper.c
@@ -1,75 +1,4 @@
 #include "monty.h"
-
-/*void opcode_function(stack_t **stack, unsigned int line_number)*/
-
-/**
- * m_push - prints the stack
- * @stack: stack head
- * @line_number: no used
- * Return: no return
-*/
-
-void m_push(stack_t **stack, unsigned int line_number)
-{
-	if (isdigit(line_number))
-	{
-		stack_t *node = malloc(sizeof(stack_t));
-
-		if (node == NULL)
-		{
-			fprintf(stderr, "Error: malloc failed\n");
-			exit(EXIT_FAILURE);
-		}
-		node->n = line_number;
-		node->prev = NULL;
-		node->next = *stack;
-		if (*stack != NULL)
-		{
-			(*stack)->prev = node;
-		}
-		*stack = node;
-		printf("%d\n", line_number);
-		free(node);
-	}
-	else
-	{
-		fprintf(stderr, "L%u: usage: push integer\n", line_number);
-		exit(EXIT_FAILURE);
-	}
-}
-/**
- * m_pall - prints the stack
- * @stack: stack head
- * @line_number: no used
-*/
-void m_pall(stack_t **stack, unsigned int line_number)
-{
-	stack_t *h;
-	(void)line_number;
-
-	h = *stack;
-	if (h == NULL)
-		return;
-	while (h)
-	{
-		printf("%d\n", h->n);
-		h = h->next;
-	}
-}
-/**
- * m_pint - prints the stack
- * @stack: stack head
- * @line_number: no used
-*/
-void m_pint(stack_t **stack, unsigned int line_number)
-{
-	if (*stack == NULL)
-	{
-		fprintf(stderr, "L%u: can't pint, stack empty\n", line_number);
-		exit(EXIT_FAILURE);
-	}
-	printf("%d\n", (*stack)->n);
-}
 /**
  * handle_opcode - handle the opcade
  * @filename: the file
@@ -79,7 +8,7 @@ void handle_opcode(char *filename)
 	char opcode[100];
 	FILE *file = fopen(filename, "r");
 	instruction_t instructions[] = {
-		{"push", m_push},
+		/*{"push", m_push},*/
 		{"pint", m_pint},
 		{"pall", m_pall},
 	};
@@ -98,6 +27,10 @@ void handle_opcode(char *filename)
 
 		for (i = 0; i < sizeof(instructions) / sizeof(instructions[0]); i++)
 		{
+			if (strncmp(opcode, "push ", 5) == 0)
+			{
+				m_push(&stack, line_number);
+			}
 			if (strcmp(opcode, instructions[i].opcode) == 0)
 			{
 				instructions[i].f(&stack, line_number);
---
+++ opcode_func.c
@@ -0,0 +1,71 @@
+#include "monty.h"
+
+/*void opcode_function(stack_t **stack, unsigned int line_number)*/
+
+/**
+ * m_push - prints the stack
+ * @stack: stack head
+ * @line_number: no used
+ * Return: no return
+*/
+
+void m_push(stack_t **stack, unsigned int line_number)
+{
+        if (isdigit(line_number))
+        {
+                stack_t *node = malloc(sizeof(stack_t));
+
+                if (node == NULL)
+                {
+                        fprintf(stderr, "Error: malloc failed\n");
+                        exit(EXIT_FAILURE);
+                }
+                node->n = line_number;
+                node->prev = NULL;
+                node->next = *stack;
+                if (*stack != NULL)
+                {
+                        (*stack)->prev = node;
+                }
+                *stack = node;
+                printf("%d\n", line_number);
+                free(node);
+        }
+        else
+        {
+                fprintf(stderr, "L%u: usage: push integer\n", line_number);
+                exit(EXIT_FAILURE);
+        }
+}
+/**
+ * m_pall - prints the stack
+ * @stack: stack head
+ * @line_number: no used
+*/
+void m_pall(stack_t **stack, unsigned int line_number)
+{
+        stack_t *h;
+        (void)line_number;
+
+        h = *stack;
+        if (h == NULL)
+                return;
+        while (h)
+        {
+                printf("%d\n", h->n);
+                h = h->next;
+        }
+}
+/**
+ * m_pint - prints the stack
+ * @stack: stack head
+ * @line_number: no used
+*/
+void m_pint(stack_t **stack, unsigned int line_number)
+{
+        if (*stack == NULL)
+        {
+                fprintf(stderr, "L%u: can't pint, stack empty\n", line_number);
+                exit(EXIT_FAILURE);
+        }
+} 
